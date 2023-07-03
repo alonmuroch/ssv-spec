f79ecacc6da0e46c7cbe9c7009846e6cb4607876
Gal Rogozinski <galrogogit@gmail.com>
2023-07-03 22:15:19 +0300

    consensus not started

--- ssv/spectest/tests/runner/duties/newduty/consensus_not_started.go
+++ ssv/spectest/tests/runner/duties/newduty/consensus_not_started.go
@@ -24,45 +24,43 @@ func ConsensusNotStarted() tests.SpecTest {
 		Name: "new duty consensus not started",
 		Tests: []*StartNewRunnerDutySpecTest{
 			{
-				Name:                    "sync committee aggregator",
-				Runner:                  startRunner(testingutils.SyncCommitteeContributionRunner(ks), &testingutils.TestingSyncCommitteeContributionDuty),
-				Duty:                    &testingutils.TestingSyncCommitteeContributionDuty,
-				PostDutyRunnerStateRoot: "29862cc6054edc8547efcb5ae753290971d664b9c39768503b4d66e1b52ecb06",
+				Name:   "sync committee aggregator",
+				Runner: startRunner(testingutils.SyncCommitteeContributionRunner(ks), &testingutils.TestingSyncCommitteeContributionDuty),
+				Duty:   &testingutils.TestingSyncCommitteeContributionNexEpochDuty,
 				OutputMessages: []*types.SignedPartialSignatureMessage{
-					testingutils.PreConsensusContributionProofMsg(ks.Shares[1], ks.Shares[1], 1, 1), // broadcasts when starting a new duty
+					testingutils.PreConsensusContributionProofNextEpochMsg(ks.Shares[1], ks.Shares[1], 1, 1),
+					// broadcasts when starting a new duty
 				},
 			},
 			{
-				Name:                    "sync committee",
-				Runner:                  startRunner(testingutils.SyncCommitteeRunner(ks), &testingutils.TestingSyncCommitteeDuty),
-				Duty:                    &testingutils.TestingSyncCommitteeDuty,
-				PostDutyRunnerStateRoot: "5b941fd17da3e3dc11003611dedc70f6ecb4f647f2951a47ed11990bcd1f5afc",
-				OutputMessages:          []*types.SignedPartialSignatureMessage{},
+				Name:           "sync committee",
+				Runner:         startRunner(testingutils.SyncCommitteeRunner(ks), &testingutils.TestingSyncCommitteeDuty),
+				Duty:           &testingutils.TestingSyncCommitteeDutyNextEpoch,
+				OutputMessages: []*types.SignedPartialSignatureMessage{},
 			},
 			{
-				Name:                    "aggregator",
-				Runner:                  startRunner(testingutils.AggregatorRunner(ks), &testingutils.TestingAggregatorDuty),
-				Duty:                    &testingutils.TestingAggregatorDuty,
-				PostDutyRunnerStateRoot: "c54e71de23c3957b73abbb0e7b9e195b3f8f6370d62fbec256224faecf177fee",
+				Name:   "aggregator",
+				Runner: startRunner(testingutils.AggregatorRunner(ks), &testingutils.TestingAggregatorDuty),
+				Duty:   &testingutils.TestingAggregatorDutyNextEpoch,
 				OutputMessages: []*types.SignedPartialSignatureMessage{
-					testingutils.PreConsensusSelectionProofMsg(ks.Shares[1], ks.Shares[1], 1, 1), // broadcasts when starting a new duty
+					testingutils.PreConsensusSelectionProofNextEpochMsg(ks.Shares[1], ks.Shares[1], 1, 1),
+					// broadcasts when starting a new duty
 				},
 			},
 			{
-				Name:                    "proposer",
-				Runner:                  startRunner(testingutils.ProposerRunner(ks), testingutils.TestingProposerDutyV(spec.DataVersionBellatrix)),
-				Duty:                    testingutils.TestingProposerDutyV(spec.DataVersionBellatrix),
-				PostDutyRunnerStateRoot: "56eafcb33392ded888a0fefe30ba49e52aa00ab36841cb10c9dc1aa2935af347",
+				Name:   "proposer",
+				Runner: startRunner(testingutils.ProposerRunner(ks), testingutils.TestingProposerDutyV(spec.DataVersionCapella)),
+				Duty:   testingutils.TestingProposerDutyNextEpochV(spec.DataVersionCapella),
 				OutputMessages: []*types.SignedPartialSignatureMessage{
-					testingutils.PreConsensusRandaoMsgV(ks.Shares[1], 1, spec.DataVersionBellatrix), // broadcasts when starting a new duty
+					testingutils.PreConsensusRandaoNextEpochMsgV(ks.Shares[1], 1, spec.DataVersionCapella),
+					// broadcasts when starting a new duty
 				},
 			},
 			{
-				Name:                    "attester",
-				Runner:                  startRunner(testingutils.AttesterRunner(ks), &testingutils.TestingAttesterDuty),
-				Duty:                    &testingutils.TestingAttesterDuty,
-				PostDutyRunnerStateRoot: "1d42abde3ed6e27699960aa7476bb672a5c9f74f466896560f35597b56083853",
-				OutputMessages:          []*types.SignedPartialSignatureMessage{},
+				Name:           "attester",
+				Runner:         startRunner(testingutils.AttesterRunner(ks), &testingutils.TestingAttesterDuty),
+				Duty:           &testingutils.TestingAttesterDutyNextEpoch,
+				OutputMessages: []*types.SignedPartialSignatureMessage{},
 			},
 		},
 	}
