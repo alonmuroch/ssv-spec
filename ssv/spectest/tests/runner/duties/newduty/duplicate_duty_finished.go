ad8e9e1083ff13e23423d59322daf13214b40b65
Gal Rogozinski <galrogogit@gmail.com>
2023-07-03 13:02:02 +0300

    add stubs for missing tests

---
+++ ssv/spectest/tests/runner/duties/newduty/duplicate_duty_finished.go
@@ -0,0 +1,9 @@
+package newduty
+
+// DuplicateDutyFinished is a test that runs the following scenario:
+// - Runner is assigned a duty
+// - Runner finishes the duty
+// - Runner is assigned the same duty again
+func DuplicateDutyFinished() {
+	panic("implement")
+}
